b8136117f5d387a7ac416cb76a44bd9a09cfa907
Merl <merl111@protonmail.com>
2018-11-18 22:30:24 +0100

    removed NeoPromise, added top level index.ts

--- src/Neo/index.ts
+++ src/Neo/index.ts
@@ -1,24 +1,9 @@
-//import * as BigInteger from "./BigInteger";
-//import * as Cryptography from "./Cryptography";
-//import * as Helper from "./Helper";
-//import * as Map from "./Map";
-//import * as IO from "./IO";
-//import * as Fixed8 from "./Fixed8";
-//import * as Promise from "./Promise";
-//import * as Uint160 from "./Uint160";
-//import * as Uint256 from "./Uint256";
-//import * as Uint64 from "./Uint64";
-//import * as UintVariable from "./UintVariable";
-//
-//export { BigInteger, Cryptography, Helper, Map, IO, Fixed8, Promise, Uint160, Uint256, Uint64, UintVariable }
-
 import * as Cryptography from "./Cryptography";
 import * as IO from "./IO";
 
 export * from "./BigInteger";
 export * from "./Map";
 export * from "./Fixed8";
-export * from "./Promise";
 export * from "./Uint160";
 export * from "./Uint256";
 export * from "./Uint64";
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as ThinNeo from "./ThinNeo";
+import * as Neo from "./Neo";
+
+export { ThinNeo, Neo };
+
